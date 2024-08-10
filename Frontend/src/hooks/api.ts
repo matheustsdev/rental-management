cc59c365c6227dc3a6b89058f5efe65f6427b705
Matheus Teixeira <matheusts.dev@gmail.com>
2024-08-10 19:51:14 -0300

    backend(all): update application folder place to better start simultaneos with frontend

--- Frontend/src/hooks/api.ts
+++ Frontend/src/hooks/api.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://localhost:7024/api",
-    headers: {
-        "Content-Type": "application",
-        "Accept": "application/json",
-        "Access-Control-Allow-Origin": "*"
-    }
-});
+    baseURL: "https://localhost:7024/api"
+}); 
