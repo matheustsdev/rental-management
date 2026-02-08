1155877d0fec00848f17898c638fd3e6d655fc2d
Matheus Teixeira <matheusts.dev@gmail.com>
2026-02-08 09:37:59 -0300

    feat: partial refactor of availability

--- src/core/infrastructure/database/PrismaProductRepository.ts
+++ src/core/infrastructure/database/PrismaProductRepository.ts
@@ -3,6 +3,8 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { IProductRepository, ProductListInput } from "@/core/domain/repositories/IProductRepository";
 import { Product } from "@/core/domain/entities/Product";
 import { ProductType } from "@/types/entities/ProductType";
+import { ProductAvailabilityType } from "@/types/ProductAvailabilityType";
+import { EAvailabilityStatus } from "@/constants/EAvailabilityStatus";
 
 
 export class PrismaProductRepository implements IProductRepository {
@@ -30,6 +32,64 @@ export class PrismaProductRepository implements IProductRepository {
       }
     });
   }
+
+  // async listWithAvailability(params: ProductListInput, startDate: Date, endDate: Date): Promise<ProductType[]> {
+  //   const { where, orderBy, ascending, page = 1, pageSize = 10 } = params;
+  //   const skip = (page - 1) * pageSize;
+  //   const orderDirection = ascending ? "asc" : "desc";
+  //   const orderByKey = orderBy || "updated_at";
+
+  //   const products = await this.prisma.products.findMany({
+  //     where,
+  //     skip,
+  //     take: pageSize,
+  //     orderBy: {
+  //       [orderByKey]: orderDirection,
+  //     },
+  //     include: {
+  //       categories: true,
+  //       rent_products: {
+  //         where: {
+  //           deleted: false,
+  //           OR: [
+  //             {
+  //               AND: [
+  //                 {
+  //                   actual_return_date: { not: null }
+  //                 },
+  //                 {
+  //                   actual_return_date: { gte: new Date() }
+  //                 }
+  //               ]
+  //             },
+  //             {
+  //               rents: {
+  //                 return_date: {
+  //                   gte: new Date()
+  //                 }
+  //               }
+  //             }
+  //           ]
+  //         }
+  //       }
+  //     }
+  //   });
+
+  //   const productWithAvailability = products.map((product): ProductAvailabilityType => {
+  //     const isAvailable = product.rent_products.length === 0;
+
+  //     return {
+  //       ...product,  
+  //       current_rent_id: rentInUse?.id ?? null,
+  //       actual_return_date: rentInUse?.return_date ?? null,
+  //       availability: isAvailable ? EAvailabilityStatus.AVAILABLE : EAvailabilityStatus.UNAVAILABLE,
+  //       buffer_end_date: rentProduct?.actual_return_buffer_days ? new Date(rentProduct?.actual_return_buffer_days) : null,
+  //       current_rent_return_date: rentInUse?.return_date ? new Date(rentInUse.return_date) : null
+  //     }
+  //   });
+
+  //   return productWithAvailability;
+  // }
 
   async update(id: string, data: Prisma.productsUpdateInput): Promise<ProductType> {
     return this.prisma.products.update({
